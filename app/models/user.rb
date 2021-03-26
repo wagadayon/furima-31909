65e51939a630883c114d0ad3eb792a0521152f66
wagadayon <wagadayon@gmail.com>
2021-03-26 15:03:33 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    
+   validates :nickname, :email, presence: true, uniqness:{case_sensitive: true}
+   validates :password, presence: true, length: {minimum:7}
+
+   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+   validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角で入力してください" }
+
+   VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX, message: "は全角カナで入力してください" }
+
+
+  - has_many :items
+  - has_many :comments
+  - has_many :buys
   
-
-
-
 
 
 end
