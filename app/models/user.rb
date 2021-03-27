5cde4cfb7a3041c5bc0a3a8c0af0b5c02af89c2f
wagadayon <wagadayon@gmail.com>
2021-03-27 18:16:19 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,16 @@ class User < ApplicationRecord
    
    validates :nickname, presence: true
    validates :email, presence: true
-   validates :encrypted_password, presence: true, length: {minimum:7}
+   validates :encrypted_password, presence: true, length: {minimum:6}
 
-   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-   validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角で入力してください" }
-
-   VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX, message: "は全角カナで入力してください" }
-
+   with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナを使用してください" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナを使用してください" }
+    validates :birth_day
+    validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, message: "英数字文字6以上"}
 
 
 
