03b66f984ddf17e3e5399e5c701aa5b99d657e5c
wagadayon <wagadayon@gmail.com>
2021-03-27 17:56:19 +0900

    ユーザー管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  
-  devise_for :views
-  devise_for :users
-  
-   root to: "homepage#index"
+    devise_for :users
+    root to: 'items#index'
+    resources :items do
+      resources :buys, only:[:index, :create]
+      
 
-end
+      resources :users, only: [:show, :edit, :update] do
+        resources :addresses, only: [:edit, :update]
+      end
+      resources :credits, only: [:new, :index, :destroy, :create]
+    end
+  end
