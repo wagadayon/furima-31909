0351de606009087400a22d645a0739426a88136d
wagadayon <wagadayon@gmail.com>
2021-03-29 10:39:03 +0900

    update

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
-
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"0000aaaa"}
-    password_confirmation {"0000aaaa"}
-    last_name             {"太郎"}
-    first_name            {"阿部"}
-    last_name_kana        {"たろう"}
-    first_name_kana       {"あべ"}
+    
+    nickname            {"太郎"}  
+    email               {"sample@gmail.com"}          
+    last_name           {"太郎"} 
+    first_name          {"山田"}
+    last_name_kana      {"タロウ"}
+    first_name_kana     {"ヤマダ"}
+    birthday            {"1996-3-10"}
+    encrypted_password   {"0000000"}
   end
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,83 +1,107 @@
 
 require 'rails_helper'
-
-
-
 describe User do
-
   describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+      end
 
-    it "ネームの入力が空かどうか" do
-      user = build(:user, name: "")
+    it "is invalid without a nickname" do
+      user = build(:user, nickname: "")
       user.valid?
-      expect(user.errors[:name]).to include("を入力してください")
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
 
-    it "Eメールの入力が空かどうか" do
+    it "is invalid without a email" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
 
-    it "パスワードの入力が空かどうか" do
+    it "is invalid without a password" do
       user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
 
-    it "Eメールは重複して登録されていないかどうか" do
-      user = create(:user, email: "sample@sample.com")
-      another_user = build(:user, email: "sample@sample.com")
+    it "is invalid without a password_confirmation" do
+      user = build(:user,  encrypted_confirmation: "")
+      user.valid?
+      expect(user.errors[:encrypted_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    it "is invalid without a first_name" do
+      user = build(:user, first_name: "")
+      user.valid?
+      expect(user.errors[:first_name]).to include("を入力してください")
+    end
+
+    it "is invalid with a first_name by Half-width alphanumeric characters" do
+      user = build(:user, first_name: "kotaro")
+      user.valid?
+      expect(user.errors[:first_name]).to include("は、全角で入力して下さい")
+    end
+
+    it "is invalid without a family_name" do
+      user = build(:user, family_name: "")
+      user.valid?
+      expect(user.errors[:family_name]).to include("を入力してください")
+    end
+
+    it "is invalid with a family_name by Half-width alphanumeric characters" do
+      user = build(:user, family_name: "hurima")
+      user.valid?
+      expect(user.errors[:family_name]).to include("は、全角で入力して下さい")
+    end
+
+    it "is invalid without a first_name_kana" do
+      user = build(:user, first_name_kana: "")
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("を入力してください")
+    end
+
+    it "is invalid with a first_name_kana by Half-width alphanumeric characters and Kanji" do
+      user = build(:user, first_name_kana: "小太郎")
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("は、全角カナで入力して下さい")
+    end
+
+    it "is invalid without a family_name_kana" do
+      user = build(:user, family_name_kana: "")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("を入力してください")
+    end
+
+    it "is invalid with a family_name_kana by Half-width alphanumeric characters and Kanji" do
+      user = build(:user, family_name_kana: "小太郎")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("は、全角カナで入力して下さい")
+    end
+
+    
+
+    it "is valid with more than 7 characters in password" do
+      user = build(:user, password: "1234567", password_confirmation: "1234567")
+      expect(user).to be_valid
+    end
+
+    it "is invalid too short a password" do
+      user = build(:user, password: "123456", password_confirmation: "123456")
+      user.valid?
+      expect(user.errors[:password]).to include("は7文字以上で入力してください")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
       another_user.valid?
       expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
-    it "パスワードが７文字以下の場合登録できないこと" do
-      user = build(:user, password: "123456")
+    it 'is invalid too long a password' do
+      user = build(:user, password: 'testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128testtoolong128')
       user.valid?
-      expect(user.errors[:password]).to include("は7文字以上で入力してください")
+      expect(user.errors[:password]).to include("は128文字以内で入力してください")
     end
-    
-    it "パスワードが７文字以上の場合登録できること" do
-      user = build(:user, password: "1234567")
-      expect(user).to be_valid
-    end
-
   end
-
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
