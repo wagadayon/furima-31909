24f09a32692e17006db0c9c4f8003afdbe721b5b
wagadayon <wagadayon@gmail.com>
2021-04-01 12:22:52 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
    
-   
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         
    
 
    with_options presence: true do
