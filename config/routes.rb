e235e9aa46a0106ed0c05474b39c198a823282ab
wagadayon <wagadayon@gmail.com>
2021-04-03 15:02:40 +0900

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
  
-  devise_for :users
-    root to: 'items#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get 'sending_destinations', to: 'users/registrations#newSendingDestination'
+    post 'sending_destinations', to: 'users/registrations#createSendingDestination'
+  end
+   
+  root to: 'items#index'
     
     resources :items, only: [:new, :create, :update] do
       collection do
