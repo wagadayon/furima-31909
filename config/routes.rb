895b4e8f4f7b38b150b77f38668bfb8cf9a2cc44
wagadayon <wagadayon@gmail.com>
2021-04-03 14:38:13 +0900

    商品出品機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+ 
+  devise_for :users
+    root to: 'items#index'
+    
+    resources :items, only: [:new, :create, :update] do
+      collection do
+        get 'get_category_child', to: 'items#get_category_child', defaults: { format: 'json' }
+        get 'get_category_grandchild', to: 'items#get_category_grandchild', defaults: { format: 'json' }
+      end
+    end
