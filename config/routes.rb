b50dfaf2d473535517f4871fc51d4417cda6305b
wagadayon <wagadayon@gmail.com>
2021-04-16 18:00:00 +0900

    up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     devise_for :users
     get 'items/index'
     root to: "items#index"
-    resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :items, [:new, :create, :show, :edit, :update, :destroy] do
       resources :purchases, only: [:index, :create]
     end
   end
